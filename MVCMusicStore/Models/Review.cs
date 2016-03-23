c44a58a926306763c9182c439dea213bd4c7877e
ashuron <nrxfUzXcQtZNHEjqzpFp>
2016-03-23 18:03:17 +0300

    Initial commit

---
+++ MVCMusicStore/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+
+        public int AlbumId { get; set; }
+
+        //public virtual Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+        [Display(Name ="Email address")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+    }
+}
